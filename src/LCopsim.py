946d476c1199ece751b7a55786455e42eaa062ce
Cory Crice <fjordwizard@gmail.com>
2016-12-15 15:04:18 -0500

    Added SQLite functionality, needs 'GROUP By field' changeds and needs to work with form data as well

--- src/LCopsim.py
+++ src/LCopsim.py
@@ -1,18 +1,4 @@
 #!/usr/bin/env python
-'''
-LCpage feeds form data to the LCsim package
-
-LCpage accepts form data and transforms it into argument readable by other
-programs in the LCsim package. LCmain is called as a subprocess to handle
-all the data and return the finished product to a downloadable file.
-
-@package LCsim
-@author blairlm
-
-'''
-# -----------------------------
-# Standard library dependencies
-# -----------------------------
 
 import cgi 
 import cgitb
@@ -21,16 +7,10 @@ from subprocess import Popen, PIPE
 import sys
 from time import time, gmtime, strftime
 import numpy as np
-
-#--------------------------------
-#Third party library dependencies
-#--------------------------------
-
-#from astropy import SkyCoord
-
-# --------------------
-# Function Definitions
-# --------------------
+import sqlite3
+import argparse
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 
 def saveFile(ufile, saveDir):
         fpath = "%s/%s" % (saveDir, uFile.filename)
@@ -44,43 +24,81 @@ def saveFile(ufile, saveDir):
         sFile.write(buf)
         sFile.close()
         
-# -------------
-# Main Function
-# -------------
+#Connect to database
+tic_con = sqlite3.connect('/home/student/research/OpSim/minion_1016_sqlite.db')
 
-def LCopsim_main():
+#Create Cursor
+ticcurs = tic_con.cursor()
+#Build SQL query
+ticcmd = 'SELECT fieldID,fieldRA,fieldDec FROM field'
     
-    index = np.array([-1.570795,-1.483528611,-1.396262222,-1.308995833,-1.221729444,-1.134463056,-1.047196667,
-                  -0.959930278,-0.872663889,-0.7853975,-0.733037667,-0.680677833,-0.628318,-0.575958167,
-                  -0.523598333,-0.4712385,-0.418878667,-0.366518833,-0.314159,-0.261799167,-0.209439333,
-                  -0.1570795,-0.104719667,-0.052359833,0,0.052359833,0.104719667,0.1570795,0.209439333,
-                  0.261799167,0.314159,0.366518833,0.418878667,0.4712385,0.523598333,0.575958167,0.628318,
-                  0.680677833])
+tic_con.row_factory = sqlite3.Row
+ticcurs.execute(ticcmd)
+#Fetch all the rows
+ticres = ticcurs.fetchall()
+
+LSSTFieldID = list()
+LSSTFieldRA = list()
+LSSTFieldDec = list()
+
+for star in ticres:
+#build lists
+    LSSTFieldID.append(star[0])
+    LSSTFieldRA.append(star[1])
+    LSSTFieldDec.append(star[2])
+        
+print "Number of Fields: {}".format(len(LSSTFieldID))
+LSSTFieldID_arr = np.array(LSSTFieldID)
+LSSTFieldRA_arr =  np.array(LSSTFieldRA)
+LSSTFieldDec_arr = np.array(LSSTFieldDec)
+print(LSSTFieldID_arr)
+print(LSSTFieldRA_arr)
+print(LSSTFieldDec_arr)
+
+ticcurs.close()
+
+def LCopsim_main(ra,dec,filtr):
+        
+    Target = SkyCoord(ra*u.deg,dec*u.deg, frame='icrs')
     
-    # Create instance of FieldStorage 
+    Hexagon_compare = SkyCoord(LSSTFieldRA_arr*u.deg, LSSTFieldDec_arr*u.deg, frame='icrs')
+    Hexagon_minID = np.argmin(SkyCoord.separation(Target,Hexagon_compare))
+    print(Hexagon_minID)
+  
+    tic_con2 = sqlite3.connect('/home/student/research/OpSim/minion_1016_sqlite.db')
+    ticcurs2 = tic_con.cursor()
+    ticcmd2 = 'SELECT filter,expMJD FROM summary GROUP BY fieldID'
+    tic_con2.row_factory = sqlite3.Row
+    ticcurs2.execute(ticcmd2)
+    ticres2 = ticcurs2.fetchall()
+
+    LSSTFieldFilter = list()
+    LSSTFieldDate = list()
+    
+    for star in ticres2:
+        LSSTFieldFilter.append(star[0])
+        LSSTFieldDate.append(star[1])
+        
+        LSSTFieldFilter_arr = np.array(LSSTFieldFilter)
+        LSSTFieldDate_arr =  np.array(LSSTFieldDate)
+
+    ticcurs2.close()
+    
+    print(LSSTFieldFilter_arr[Hexagon_minID])
+    print(LSSTFieldDate_arr[Hexagon_minID])
+
     form = cgi.FieldStorage()
     
-    #Create directory with timestamp down to the second
-    success = False
-    while not success:
-        name = strftime("%Y_%m_%d_%H_%M_%S", gmtime())
-        if not os.path.isdir(name):
-            success = True
-    os.umask(0000)
-    os.mkdir('storage3/{}'.format(name))
-    
-    #Process regular inputs
-        
     if form.getvalue('ra'):
         ra = form.getvalue('ra')
-        ra = float(ra)*np.pi/360
+        ra = float(ra)*np.pi/180
     else:
         print 'You must specify a Right Ascension. Please go back.'
         sys.exit(0)
     
     if form.getvalue('dec'):
         dec = form.getvalue('dec')
-        dec = float(dec)*np.pi/360
+        dec = float(dec)*np.pi/180
     else:
         print 'You must specify a declination. Please go back.'
         sys.exit(0)
@@ -89,52 +107,27 @@ def LCopsim_main():
         print 'The LSST is a Southern Hemisphere survey, so there are less observations the higher the declination.<br>'
     if dec > .305432:
         print 'There are fewer observations above 35 degrees, so the closest observation may be more than a degree away or more.<br>'
-    
-    #Process dropdown box
-    
-    type = form.getvalue('filter')
-    
-    opsim = np.genfromtxt('/hd1/LCsim/opsim/opsimindex.dat')
-    
-    osidx = [0]*opsim.shape[0]
-    
-    for i in range(opsim.shape[0]):
-        osidx[i] = 2*np.arcsin(np.sqrt(np.square(np.sin((opsim[i,3]-dec)/2))+np.cos(dec)*np.cos(opsim[i,3])*np.square(np.sin((ra-opsim[i,2])/2))))
-    
-    minimum = min(osidx)
-    idx = osidx.index(minimum)
-    
-    val = 1
-    
-    while opsim[idx,3] > index[val]:
-        val = val + 1
-        
-    osfinal = np.genfromtxt('/hd1/LCsim/opsim/opsim{:02d}.txt'.format(val-1), dtype="i4,f,f,f,S1,S7")
-    
-    outfile = open('/hd1/LCsim/storage3/{}/opsimobservation.txt'.format(name),'w')
-    
-    outfile.write('#Observation in MJD\n')
-    for line in osfinal:
-        if np.abs(line[3] - opsim[idx,3]) < .00001 and np.abs(line[2] - opsim[idx,2]) < .00001 and type == line[4]:
-            outfile.write("{:.5f}\n".format(line[1]))
-        elif np.abs(line[3] - opsim[idx,3]) < .00001 and np.abs(line[2] - opsim[idx,2]) < .00001 and type == 'all':
-            outfile.write("{:.5f}\n".format(line[1]))
-            
-    outfile.close()
-    
-    os.chdir('/hd1/LCsim/storage3/{}'.format(name))
-    print 'The closest point was found at an Right Ascension of {:.04f} degrees and a Declination of {:.04f} degrees, which is {:.06f} degrees away.'.format(opsim[idx,2]*360/np.pi,opsim[idx,3]*360/np.pi,float(dis)*360/np.pi)
-    print '<br>'
-    print "<a href='{}' type='text/plain'> Click here for observation data! </a>".format('storage3/{}/opsimobservation.txt'.format(name))
-    print ' (Right-click to save)'
-    
-    print '''
-    </body>
-    </html>
-    '''
+  
+    filtr = form.getvalue('filtr')
 
+#    Target = SkyCoord(ra*u.rad,dec*u.rad, frame='icrs')
     
+#    for i in LSSTFieldRA_arr and LSSTFieldDec_arr:
+#        Hexagon_compare = SkyCoord(LSSTFieldRA_arr[i]*u.deg, LSSTFieldDec_arr[i]*u.deg, frame='ircs')
+#        Hexagon_min = min(SkyCoord.separation(Target,Hexagon_compare))
+#        print(Hexagon_min.ra)
+#        print(Hexagon_min.dec)
+        
 if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Simulates observation dates for LSST in a particular filter')
+    parser.add_argument('ra',type=float,help='Right ascension of target')
+    parser.add_argument('dec', type=float,help='Declination of target')
+    parser.add_argument('filtr', type=str,help='Filter used to view target')
+
+    #Put this in a dictionary
+    args = vars(parser.parse_args())
+    LCopsim_main(args['ra'],args['dec'],args['filtr'])
     
     #Output errors to web
     cgitb.enable()
