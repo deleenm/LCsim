c72eb25e9775a8e14d0484409577d80f028b500a
Diana Coronado <diana.a.coronado@gmail.com>
2016-12-30 17:34:58 -0500

    Error Catchers
    Added the flux and degree error handlers to the html code

--- html/js/errorHandlers.js
+++ html/js/errorHandlers.js
@@ -1,8 +1,66 @@
-	
-	var errorBoolean = false;
-	
-	
-	function errorHandlerPhase(inputID,outputID) {
+var errorBoolean = false;
+
+function errorHandlerFlux(inputID,outputID) {
+    var error, x;
+    error = document.getElementById(outputID);
+    error.innerHTML = "";
+    x = document.getElementById(inputID).value;
+    try { 
+        if(x == "")  throw "empty";
+        if(isNaN(x)) throw "not a number";
+        x = Number(x);
+        if(x <= -3e26)    throw "too low";
+        if(x >= 3e26 )   throw "too high";
+		else{errorBoolean= false;}
+    }
+    catch(err) {
+        error.innerHTML = "Input is " + err;
+		errorBoolean = true;
+    }	
+}
+
+
+function errorHandlerRightAscension(inputID,outputID) {
+    var error, x;
+    error = document.getElementById(outputID);
+    error.innerHTML = "";
+    x = document.getElementById(inputID).value;
+    try { 
+        if(x == "")  throw "empty";
+        if(isNaN(x)) throw "not a number";
+        x = Number(x);
+        if(x <= 0)    throw "too low";
+        if(x >= 360 )   throw "too high";
+		else{errorBoolean= false;}
+    }
+    catch(err) {
+        error.innerHTML = "Input is " + err;
+		errorBoolean = true;
+    }	
+}
+
+
+function errorHandlerRightDeclination(inputID,outputID) {
+    var error, x;
+    error = document.getElementById(outputID);
+    error.innerHTML = "";
+    x = document.getElementById(inputID).value;
+    try { 
+        if(x == "")  throw "empty";
+        if(isNaN(x)) throw "not a number";
+        x = Number(x);
+        if(x <= -90)    throw "too low";
+        if(x >= 90 )   throw "too high";
+		else{errorBoolean= false;}
+    }
+    catch(err) {
+        error.innerHTML = "Input is " + err;
+		errorBoolean = true;
+    }	
+}
+
+
+function errorHandlerPhase(inputID,outputID) {
     var error, x;
     error = document.getElementById(outputID);
     error.innerHTML = "";
@@ -20,7 +78,7 @@
 		errorBoolean = true;
     }
 }
-	function errorHandler(inputID,outputID) {
+function errorHandler(inputID,outputID) {
     var error, x;
     error = document.getElementById(outputID);
     error.innerHTML = "";
@@ -40,7 +98,8 @@
 	
 
 }
-	function errorHandlerLightObs(inputID,outputID) {
+
+function errorHandlerLightObs(inputID,outputID) {
     var error, x;
     error = document.getElementById(outputID);
     error.innerHTML = "";
