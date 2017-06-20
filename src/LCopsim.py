602314dc0de463bf79e1ce1426ee97d78ceca285
Nathan De Lee <deleenm@nku.edu>
2017-06-20 17:46:30 -0400

    Fixed LCopsim.py so that it now runs and displays output.

--- src/LCopsim.py
+++ src/LCopsim.py
@@ -16,132 +16,138 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 
 def saveFile(ufile, saveDir):
-        fpath = "%s/%s" % (saveDir, uFile.filename)
-        buf = uFile.file.read()
-        bytes = len(buf)
-        try:
-            sFile = open(fPath, 'wb')
-        except I0Error:
-            print "File {} could not be opened!".format(fPath)
-            sys.exit(1)
-        sFile.write(buf)
-        sFile.close()
+    fpath = "%s/%s" % (saveDir, uFile.filename)
+    buf = uFile.file.read()
+    bytes = len(buf)
+    try:
+        sFile = open(fPath, 'wb')
+    except I0Error:
+        print "File {} could not be opened!".format(fPath)
+        sys.exit(1)
+    sFile.write(buf)
+    sFile.close()
         
-#Connect to database
-tic_con = sqlite3.connect('../data/minion_1016_sqlite.db')
+def LCopsim_main(cra,cdec,cfiltr):
+    #Connect to database
+    tic_con = sqlite3.connect('../data/minion_1016_sqlite.db')
 
-#Create Cursor
-ticcurs = tic_con.cursor()
-#Build SQL query
-ticcmd = 'SELECT fieldID,fieldRA,fieldDec FROM field'
-    
-tic_con.row_factory = sqlite3.Row
-ticcurs.execute(ticcmd)
-#Fetch all the rows
-ticres = ticcurs.fetchall()
-
-LSSTFieldID = list()
-LSSTFieldRA = list()
-LSSTFieldDec = list()
-
-for star in ticres:
-#build lists
-    LSSTFieldID.append(star[0])
-    LSSTFieldRA.append(star[1])
-    LSSTFieldDec.append(star[2])
+    #Create Cursor
+    ticcurs = tic_con.cursor()
         
-print "Number of Fields: {}".format(len(LSSTFieldID))
-LSSTFieldID_arr = np.array(LSSTFieldID)
-LSSTFieldRA_arr =  np.array(LSSTFieldRA)
-LSSTFieldDec_arr = np.array(LSSTFieldDec)
-print(LSSTFieldID_arr)
-print(LSSTFieldRA_arr)
-print(LSSTFieldDec_arr)
+    #Build SQL query to
+    ticcmd = 'SELECT fieldID,fieldRA,fieldDec FROM field'
+    
+    tic_con.row_factory = sqlite3.Row
+    ticcurs.execute(ticcmd)
+    #Fetch all the rows
+    ticres = ticcurs.fetchall()
 
-ticcurs.close()
+    LSSTFieldID = list()
+    LSSTFieldRA = list()
+    LSSTFieldDec = list()
 
-def LCopsim_main(ra,dec,filtr):
+    for star in ticres:
+        #build lists
+        LSSTFieldID.append(star[0])
+        LSSTFieldRA.append(star[1])
+        LSSTFieldDec.append(star[2])
         
-    Target = SkyCoord(ra*u.deg,dec*u.deg, frame='icrs')
-    
-    Hexagon_compare = SkyCoord(LSSTFieldRA_arr*u.deg, LSSTFieldDec_arr*u.deg, frame='icrs')
-    Hexagon_minID = np.argmin(SkyCoord.separation(Target,Hexagon_compare))
-    print(Hexagon_minID)
-  
-    tic_con2 = sqlite3.connect('../data/minion_1016_sqlite.db')
-    ticcurs2 = tic_con.cursor()
-    ticcmd2 = 'SELECT filter,expMJD FROM summary GROUP BY fieldID'
-    tic_con2.row_factory = sqlite3.Row
-    ticcurs2.execute(ticcmd2)
-    ticres2 = ticcurs2.fetchall()
-
-    LSSTFieldFilter = list()
-    LSSTFieldDate = list()
-    
-    for star in ticres2:
-        LSSTFieldFilter.append(star[0])
-        LSSTFieldDate.append(star[1])
-        
-        LSSTFieldFilter_arr = np.array(LSSTFieldFilter)
-        LSSTFieldDate_arr =  np.array(LSSTFieldDate)
-
-    ticcurs2.close()
-    
-    print(LSSTFieldFilter_arr[Hexagon_minID])
-    print(LSSTFieldDate_arr[Hexagon_minID])
+    LSSTFieldID_arr = np.array(LSSTFieldID)
+    LSSTFieldRA_arr =  np.array(LSSTFieldRA)
+    LSSTFieldDec_arr = np.array(LSSTFieldDec)
 
     form = cgi.FieldStorage()
     
-    if form.getvalue('ra'):
+    if(cra != None):
+        ra = cra
+    elif form.getvalue('ra'):
         ra = form.getvalue('ra')
-        ra = float(ra)*np.pi/180
+        ra = float(ra)
     else:
         print('You must specify a Right Ascension. Please go back.')
         sys.exit(0)
     
+    if(cdec != None):
+        dec = cdec
     if form.getvalue('dec'):
         dec = form.getvalue('dec')
-        dec = float(dec)*np.pi/180
+        dec = float(dec)
     else:
         print('You must specify a declination. Please go back.')
         sys.exit(0)
     
     if dec > 0:
         print 'The LSST is a Southern Hemisphere survey, so there are less observations the higher the declination.<br>'
-    if dec > .305432:
+    if dec > 35:
         print 'There are fewer observations above 35 degrees, so the closest observation may be more than a degree away or more.<br>'
   
-    filtr = form.getvalue('filtr')
-
-#    Target = SkyCoord(ra*u.rad,dec*u.rad, frame='icrs')
+    if cfiltr != None:
+        filtr = cfiltr
+    elif form.getvalue('filter'):
+        filtr = form.getvalue('filter')
     
-#    for i in LSSTFieldRA_arr and LSSTFieldDec_arr:
-#        Hexagon_compare = SkyCoord(LSSTFieldRA_arr[i]*u.deg, LSSTFieldDec_arr[i]*u.deg, frame='ircs')
-#        Hexagon_min = min(SkyCoord.separation(Target,Hexagon_compare))
-#        print(Hexagon_min.ra)
-#        print(Hexagon_min.dec)
+    
+    Target = SkyCoord(ra*u.deg,dec*u.deg, frame='icrs')
+    
+    Hexagon_compare = SkyCoord(LSSTFieldRA_arr*u.deg, LSSTFieldDec_arr*u.deg, frame='icrs')
+    separation = SkyCoord.separation(Target,Hexagon_compare)
+    Hexagon_minID = np.argmin(separation)
+    fieldId = str(LSSTFieldID_arr[Hexagon_minID])
+    fieldRa = LSSTFieldRA_arr[Hexagon_minID]
+    fieldDec = LSSTFieldDec_arr[Hexagon_minID]
+    fieldSep = separation[Hexagon_minID]
+
+    if filtr == 'all':
+        ticcmd2 = 'SELECT filter,expMJD FROM summary WHERE fieldID = ?'
+        ticcurs.execute(ticcmd2,(fieldId,))
+    else:
+        ticcmd2 = ('SELECT filter,expMJD FROM summary ' 
+                  + 'WHERE fieldID = ? AND filter = ?') 
+        ticcurs.execute(ticcmd2,(fieldId,filtr))
+
+    ticres = ticcurs.fetchall()
+
+    LSSTFieldFilter = list()
+    LSSTFieldDate = list()
+    
+    for star in ticres:
+        LSSTFieldFilter.append(star[0])
+        LSSTFieldDate.append(star[1])
+        
+        LSSTFieldFilter_arr = np.array(LSSTFieldFilter)
+        LSSTFieldDate_arr =  np.array(LSSTFieldDate)
+
+    ticcurs.close()
+    
+    print('<table align="center" border="1">')
+    print("<tr><th colspan='2'>Field:{} RA: {:.3f} Dec: {:.3f} Dist: {:.3f}</th></tr>".format(fieldId,fieldRa,fieldDec,fieldSep))
+    print("<tr><th>Filter</th><th>MJD</th></tr>")
+    
+    for i in range(len(LSSTFieldFilter_arr)):
+        print("<tr><td>{}</td><td>{}</td></tr>".format(LSSTFieldFilter_arr[i],LSSTFieldDate_arr[i]))
+    
+    print("</table>")
         
 if __name__ == '__main__':
+
+    #Output errors to web
+    cgitb.enable()
     
     parser = argparse.ArgumentParser(description='Simulates observation dates for LSST in a particular filter')
-    parser.add_argument('ra',type=float,help='Right ascension of target')
-    parser.add_argument('dec', type=float,help='Declination of target')
-    parser.add_argument('filtr', type=str,help='Filter used to view target')
+    parser.add_argument('-r',metavar="RA",type=float,help='Right ascension of target')
+    parser.add_argument('-d',metavar="DEC", type=float,help='Declination of target')
+    parser.add_argument('-f', metavar="FILTER1,FILTER2",type=str,help='Filter used to view target')
 
     #Put this in a dictionary
     args = vars(parser.parse_args())
 
-    
-    #Output errors to web
-    cgitb.enable()
-    
-    #Prepare webpage
+        #Prepare webpage
     print "Content-type: text/html"
     print
     print '''<html>
     <head>
     <meta charset="UTF-8">
-    <title>LCsim Results</title>
+    <title>LCOpsim Results</title>
     <style type="text/css">
     body {
         background-color: #F4F4F4;
@@ -150,11 +156,11 @@ if __name__ == '__main__':
     </head>
     
     <body>
-    <p><img src="banner.jpg" width="600" height="199"  alt=""/></p>
+    <p><img src="../html/banner.jpg" width="600" height="199"  alt="" style="margin:auto;display:block"/></p>
     '''
     
     #Run LCopsim
-    ret = LCopsim_main(args['ra'],args['dec'],args['filtr'])
+    ret = LCopsim_main(args['r'],args['d'],args['f'])
     sys.exit(ret)
     
-    pass
+    print("</body>")
