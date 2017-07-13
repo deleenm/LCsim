8e51c7571499f3376e9025f54ec3acbc0607343f
Diana Coronado <diana.a.coronado@gmail.com>
2017-07-13 02:00:32 -0400

    u

--- src/cleaner.py
+++ src/cleaner.py
@@ -45,7 +45,8 @@ def cleaner():
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
-                # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
         except Exception as e:
             print(e)
 
