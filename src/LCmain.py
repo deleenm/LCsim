aed54fca4abc841dab04d810b971fe9240b9fd1f
Diana Coronado <diana.a.coronado@gmail.com>
2017-07-13 00:18:37 -0400

    u

--- src/LCmain.py
+++ src/LCmain.py
@@ -22,7 +22,7 @@ from subprocess import Popen
 # -------------------
 # Third-party imports
 # -------------------
-import LCsim,LCpage
+import LCsim
 import numpy as np
 # -------------
 # Main Function
@@ -238,7 +238,7 @@ def LCmain_main(args):
     tar.wait()
 
 
-    print "<a href='{}' type='text/plain'> Click here for lightcurve data! </a>".format('../storage3/{}/{}.tar.gz'.format(args.name, args.f))
+    print "<a href='{}' type='text/plain' id='link'> Click here for lightcurve data! </a>".format('../storage3/{}/{}.tar.gz'.format(args.name, args.f))
 
 
 
