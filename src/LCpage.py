fd3af245196b4a10ca4c1efabbd01add40b03a8f
Diana Coronado <diana.a.coronado@gmail.com>
2017-11-08 15:54:09 -0500

    Updates
    -Added dropdown menu for templates
    -Added tooltips
    -Added statement to LCpage.py to process the selection

--- src/LCpage.py
+++ src/LCpage.py
@@ -61,8 +61,11 @@ def LCpage_main():
     
     #Ensure that template file is uploaded
     if not form['tempfile'].filename:
-        print('You must include a template file. Please go back and upload one.')
-        sys.exit(0)
+        if form.getvalue('RadioGroup3_0') == 'useTmp':
+            print("test")
+
+    print('You must include a template file. Please go back and upload one.')
+    sys.exit(0)
     
     #Create directory with timestamp down to the second
 
